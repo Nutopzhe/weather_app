a15261fba0ae31c9ac7e6ba756df09f90c4f09d9
Nikolay Rybakov <ni_fanta@mail.ru>
2021-02-16 01:17:54 +0300

    Finished app

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,15 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.json.JSONObject;
 
 public class Controller {
 
@@ -15,7 +20,7 @@ public class Controller {
     private URL location;
 
     @FXML
-    private TextField enterCityField;
+    private TextField city;
 
     @FXML
     private Button searchButton;
@@ -37,6 +42,43 @@ public class Controller {
 
     @FXML
     void initialize() {
+        searchButton.setOnAction(event -> {
+            String getUserCity = city.getText().trim();
+            if (!getUserCity.equals("")) {
+                String output = getUrlContext("https://api.openweathermap.org/data/2.5/weather?q="
+                        + getUserCity
+                        + "&units=metric&appid=4962504a1566e0315d15a711c70cd3b6&lang=ru");
 
+                if (!output.isEmpty()) {
+                    JSONObject obj = new JSONObject(output);
+                    temp.setText(String.valueOf(obj.getJSONObject("main").getDouble("temp")));
+                    feels_like.setText(String.valueOf(obj.getJSONObject("main").getDouble("feels_like")));
+                    temp_min.setText(String.valueOf(obj.getJSONObject("main").getDouble("temp_min")));
+                    temp_max.setText(String.valueOf(obj.getJSONObject("main").getDouble("temp_max")));
+                    pressure.setText(String.valueOf(obj.getJSONObject("main").getDouble("pressure")));
+                }
+            }
+        });
+    }
+
+    private static String getUrlContext(String urlAdress) {
+        StringBuffer content = new StringBuffer();
+
+        try {
+            URL url = new URL(urlAdress);
+            URLConnection connection = url.openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+
+            while((line = br.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Такого город не найден!");
+        }
+
+        return content.toString();
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Weather app");
         primaryStage.setScene(new Scene(root, 250, 400));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
