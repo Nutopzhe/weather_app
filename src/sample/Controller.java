5b74da7a733eb67ee8a2f0c4b2b4d1fff8dd599d
Nikolay Rybakov <ni_fanta@mail.ru>
2021-02-16 00:35:37 +0300

    Front of project is created

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,42 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField enterCityField;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TextField temp;
+
+    @FXML
+    private TextField feels_like;
+
+    @FXML
+    private TextField temp_max;
+
+    @FXML
+    private TextField temp_min;
+
+    @FXML
+    private TextField pressure;
+
+    @FXML
+    void initialize() {
+
+    }
 }
